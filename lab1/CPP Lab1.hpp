4cf40327fec2ad45ecff27d29670ac462760d401
owari-no-ooutsuke <nastyavolgina158@gmail.com>
2025-02-18 13:36:40 +0300

    структуры

---
+++ lab1/CPP Lab1.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+//уравнение ax^2 + bx + c
+typedef struct {
+	double a;
+	double b;
+	double c;
+} quadEq;
+
+//корни уравнения
+typedef struct {
+	int rootCount; //количество корней
+	double* roots; //массив корней
+} eqSolution;
